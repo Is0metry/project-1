d6cc5e0dcedc4312952cecd79a06f5ce2086b5c4
Woody Sims <sims.woodrow@gmail.com>
2022-10-30 21:51:02 -0500

    comments in explore

--- explore.py
+++ explore.py
@@ -9,9 +9,11 @@ from IPython.display import Markdown as md
 from wrangle import ADD_ONS, CAT_COLS, IST_KEY, CONTRACT_KEY, PAYMENT_KEY
 
 
-def tech_support_vs_churn(train:pd.DataFrame):
+def tech_support_vs_churn(train:pd.DataFrame)->None:
+    '''tech_support_vs_churn takes the training DataFrame and shows 2
+    pie charts, one showing churn proportions for users with tech support
+    and the other with tech support'''
     fiber = train[(train.internet_service_type == 1)]
-    dsl = train[train.internet_service_type == 1]
     plt.suptitle('Fiber Optic customer churn with & Without Tech Support')
     plt.subplot(2,2,1)
     plt.title('FiOS With TS')
@@ -22,11 +24,16 @@ def tech_support_vs_churn(train:pd.DataFrame):
     plt.pie(fiber[fiber.tech_support == 0].churn.value_counts())
    
 def chi_squared(train:pd.DataFrame):
+    '''chi_squared takes the training dataset and returns an IPython.display.Markdown
+    object containg the formatted chi squared and p-value for the operation'''
     df = train[(train.internet_service_type > 0)]
     observed = pd.crosstab(df.churn, df.tech_support)
     chi2, p, degf, expected = stats.chi2_contingency(observed) 
     return md(f'$\chi^2:{chi2:.4f}$\n\n $p:{p}$')
 def churn_by_ist(train:pd.DataFrame):
+    '''churn_by_ist takes the train DataFrame and creates
+    3 pie charts indicating users who churned and did not churn
+    separated by internet_service_type.'''
     none = train[(train.internet_service_type == 0)].churn.value_counts().sort_index()
     dsl = train[(train.internet_service_type == 1)].churn.value_counts().sort_index()
     fib_op = train[(train.internet_service_type == 2)].churn.value_counts().sort_index()
@@ -43,17 +50,27 @@ def churn_by_ist(train:pd.DataFrame):
     plt.legend(['Churned','Didn\'t churn'],loc='lower right')
     plt.show()
 def add_ons_vs_tenure(train:pd.DataFrame):
+    '''add ons takes the training dataset and shows a barplot of number of add-ons
+    vs the average customer tenure'''
     plt.title('Add-ons vs Tenure')
     plt.xlabel('No. of Add-ons')
     sns.barplot(data=train,x='add_ons',y='tenure')
 def pearson_test(train:pd.DataFrame)->md:
+    '''pearson_test takes the training DataFrame and performs a pearson correlation coefficient
+    operation on the number of add_ons vs. the average customer's tenure. It returns
+    an IPython.display.Markdown object with the r and p values.'''
     x = train.add_ons
     y = train.tenure
     r,p = stats.pearsonr(x,y)
     return md(f'$ r = {r}$\n\n$p = {p}$')
-def monthly_vs_total(train:pd.DataFrame):
+def gender_vs_churn(train:pd.DataFrame):
+    '''gender_vs_churn takes the training DataFrame and shows a plot
+    showing churn counts by gender'''
     sns.countplot(data=train,x='churn',hue='is_male')
     plt.xticks([0,1],['Did Not Churn','Churned'])
     plt.legend(['Female','Male'])
 def add_ons_w_churn(train:pd.DataFrame):
+    '''add_ons_w_churn takes the training database and shows a seaborn plot
+    showing the number of customers with a certain number of add_ons grouped
+    by churn.'''
     sns.countplot(data=train,x='add_ons',hue='churn')
