d7d9294a158e84e42282aaf37ed7556bd2474309
Woody Sims <sims.woodrow@gmail.com>
2022-10-30 22:10:11 -0500

    model.py commented

--- model.py
+++ model.py
@@ -8,17 +8,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
 def prep_for_modeling(train:pd.DataFrame,validate:pd.DataFrame, test:pd.DataFrame):
+    '''prep_for_modeling takes the train, validate, and test DataFrames and returns
+    a tuple of the features and target of each DataFrame.'''
     model_features = ['add_ons','tenure','internet_service_type','tech_support']
     model_target = 'churn'
     return train[model_features],train[model_target],validate[model_features],validate[model_target],\
         test[model_features],test[model_target]
 
 def get_baseline_precision(train:pd.DataFrame, mode:int):
+    '''get_baseline_predictions takes the trainning dataset and the DataFrame mode and returns
+    the baseline precision score.'''
     mode_arr = [mode for x in range(train.shape[0])]
     return precision_score(train.churn,mode_arr)
 
 def get_decision_tree(train_x:pd.DataFrame,train_y:pd.DataFrame,\
-     valid_x:pd.DataFrame, valid_y:pd.DataFrame):
+    valid_x:pd.DataFrame, valid_y:pd.DataFrame)->md:
+    '''get_decision_tree takes in the features and target of both the 
+    train and validate DataFrames, trains a DecisionTreeClassifier
+    on the train dataset, and returns a IPython.display.Markdown
+    object with the precision scores for both the train and validate datasets'''
     tree = DecisionTreeClassifier(max_depth=5)
     tree.fit(train_x,train_y)
     train_predict = tree.predict(train_x)
@@ -30,7 +38,11 @@ def get_decision_tree(train_x:pd.DataFrame,train_y:pd.DataFrame,\
     return md(ret_str)
 
 def get_rf(train_x:pd.DataFrame,train_y:pd.DataFrame,\
-    valid_x:pd.DataFrame, valid_y:pd.DataFrame):
+    valid_x:pd.DataFrame, valid_y:pd.DataFrame)->md:
+    '''get_rf takes in the features and target of both the train and validate
+    datasets respectively, trains a RandomForestClassifier on the train dataset,
+    and returns an IPython.display.Markdown object with the precision scores
+    of both the train and validate datasets.'''
     rf = RandomForestClassifier(min_samples_leaf=100,max_depth=6)
     rf.fit(train_x,train_y)
     train_predict = rf.predict(train_x)
@@ -42,7 +54,11 @@ def get_rf(train_x:pd.DataFrame,train_y:pd.DataFrame,\
     return md(ret_str)
 
 def get_knn(train_x:pd.DataFrame,train_y:pd.DataFrame,\
-    valid_x:pd.DataFrame, valid_y:pd.DataFrame):
+    valid_x:pd.DataFrame, valid_y:pd.DataFrame)->md:
+    '''get_knn takes in the features and target of both the train and validate
+    datasets respectively, trains a KNeighborClassifier on the train dataset,
+    and returns an IPython.display.Markdown object with the precision scores
+    of both the train and validate datasets.'''
     knn = KNeighborsClassifier(n_neighbors=2)
     knn.fit(train_x,train_y)
     train_predict = knn.predict(train_x)
@@ -52,7 +68,11 @@ def get_knn(train_x:pd.DataFrame,train_y:pd.DataFrame,\
     ret_str = f'### K Nearest Neighbors precision on `train`: {precision_train}.\
         \n\n ### K Nearest Neighbors precision on `validate`: {precision_valid}.'
     return md(ret_str)
-def rf_on_test(train_x,train_y,test_x,test_y):
+def rf_on_test(train_x,train_y,test_x,test_y)->md:
+    '''rf_on_test takes in the features and targets of the train and test DataFrames,
+    trains a RandomForestClassifier model on the training data, predicts the 
+    outcome on the test data set, and returns an IPython.display.Markdown objedt
+    with the precision score on the test data set.'''
     rf = RandomForestClassifier(min_samples_leaf=100, max_depth=6)
     rf.fit(train_x, train_y)
     test_predict = rf.predict(test_x)
